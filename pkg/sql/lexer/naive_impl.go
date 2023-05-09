1091c254631fc9e60bc8d37b8694cb4d4c48928a
ducnt114 <trungduc110492@gmail.com>
2023-05-09 18:12:58 +0700

    update lexer

--- pkg/sql/lexer/naive_impl.go
+++ pkg/sql/lexer/naive_impl.go
@@ -2,14 +2,35 @@ package lexer
 
 import (
 	"github.com/ducnt114/g0database/pkg/sql"
+	"regexp"
 	"strings"
 )
 
+const (
+	pattern1 = `^varchar\([0-9][0-9]*\)$`
+	pattern2 = `^[_a-zA-Z][_a-zA-Z0-9]*$`
+	pattern3 = `^[0-9][0-9]*$`
+)
+
 type naiveLexer struct {
+	keywords []string
+	regex1   *regexp.Regexp
+	regex2   *regexp.Regexp
+	regex3   *regexp.Regexp
 }
 
 func NewNaiveLexer() Lexer {
 	res := &naiveLexer{}
+
+	res.keywords = []string{
+		"<>", ">", "<",
+		";", ",", ".", "'", "(", ")", "=", "create", "table", "select", "delete", "from", "where", "insert", "into",
+		"values", "update", "set", "drop", "column", "add", "not", "null", "primary", "key", "*", "integer", "alter",
+	}
+
+	res.regex1 = regexp.MustCompile(pattern1)
+	res.regex2 = regexp.MustCompile(pattern2)
+	res.regex3 = regexp.MustCompile(pattern3)
 
 	return res
 }
@@ -17,9 +38,65 @@ func NewNaiveLexer() Lexer {
 func (l *naiveLexer) Analyze(sqlCmd string) ([]sql.Token, error) {
 	res := make([]sql.Token, 0)
 	lowerCaseCmd := strings.ToLower(sqlCmd)
+	lowerCaseCmd = strings.ReplaceAll(lowerCaseCmd, "\n", "")
+	lowerCaseCmd = strings.ReplaceAll(lowerCaseCmd, "\t", "")
 	items := strings.Split(lowerCaseCmd, " ")
 	for _, item := range items {
-		res = append(res, sql.Token(item))
+		tokens := l.parseToken(item)
+		for _, token := range tokens {
+			res = append(res, sql.Token(token))
+		}
 	}
 	return res, nil
+}
+
+func (l *naiveLexer) parseToken(c string) []string {
+	if l.isKeyword(c) {
+		return []string{c}
+	}
+	if l.checkRegex(c) {
+		return []string{c}
+	}
+	res := make([]string, 0)
+	var startIndex = 0
+	var endIndex = len(c) - 1
+	for {
+		if endIndex < startIndex {
+			break
+		}
+		newToken := c[startIndex:endIndex]
+		tokens := l.parseToken(newToken)
+		if len(tokens) > 0 {
+			res = append(res, tokens...)
+			startIndex = endIndex
+			endIndex = len(c)
+			continue
+		} else {
+			endIndex--
+		}
+	}
+
+	return res
+}
+
+func (l *naiveLexer) isKeyword(c string) bool {
+	for _, keyword := range l.keywords {
+		if c == keyword {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *naiveLexer) checkRegex(c string) bool {
+	if l.regex1.MatchString(c) {
+		return true
+	}
+	if l.regex2.MatchString(c) {
+		return true
+	}
+	if l.regex3.MatchString(c) {
+		return true
+	}
+	return false
 }
--- pkg/sql/lexer/naive_impl_test.go
+++ pkg/sql/lexer/naive_impl_test.go
@@ -1,15 +1,79 @@
 package lexer
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
+
+func TestNaiveLexer_checkRegex(t *testing.T) {
+	l := &naiveLexer{}
+	l.regex1 = regexp.MustCompile(pattern1)
+	l.regex2 = regexp.MustCompile(pattern2)
+	l.regex3 = regexp.MustCompile(pattern3)
+	t.Run("case 1", func(t1 *testing.T) {
+		if l.checkRegex(`varchar(10)`) == false {
+			t1.Fail()
+		}
+	})
+
+}
 
 func TestNaiveLexer_Analyze(t *testing.T) {
 	l := NewNaiveLexer()
 
-	tokens, err := l.Analyze("select id, name, age from user where id > 10")
-	if err != nil {
-		t.Fail()
-	}
-	if len(tokens) <= 0 {
-		t.Fail()
-	}
+	t.Run("test case 1", func(t1 *testing.T) {
+		tokens, err := l.Analyze("select id, name, age from user where id > 10")
+		if err != nil {
+			t1.Fail()
+		}
+		for i, token := range tokens {
+			t1.Logf("tokens[%v]: %v", i, token)
+		}
+		if len(tokens) != 12 {
+			t1.Logf("expected len(tokens) = %v, actual: %v", 12, len(tokens))
+			t1.Fail()
+		}
+	})
+
+	t.Run("test case 2", func(t2 *testing.T) {
+		tokens, err := l.Analyze(`create table student( id integer not null primary key,
+			 name varchar(10) not null,
+			 hostel varchar(10)
+			);`)
+		if err != nil {
+			t2.Fail()
+		}
+		for i, token := range tokens {
+			t2.Logf("tokens[%v]: %v", i, token)
+		}
+		if len(tokens) != 20 {
+			t2.Logf("expected len(tokens) = %v, actual: %v", 20, len(tokens))
+			t2.Fail()
+		}
+	})
+
+	t.Run("test case 3", func(t3 *testing.T) {
+		tokens, err := l.Analyze(`
+			insert into student(name, hostel)
+			values(pintu, umiam);
+`)
+		if err != nil {
+			t3.Fail()
+		}
+		for i, token := range tokens {
+			t3.Logf("tokens[%v]: %v", i, token)
+		}
+		if len(tokens) != 15 {
+			t3.Logf("expected len(tokens) = %v, actual: %v", 15, len(tokens))
+			t3.Fail()
+		}
+	})
+
 }
+
+//func TestSlice(t *testing.T) {
+//	fmt.Println("id,"[0:2])
+//	fmt.Println("id,"[2:2])
+//	fmt.Println("id,"[1:2])
+//	fmt.Println("id,"[2:3])
+//}
