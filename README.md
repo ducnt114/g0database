729b03bdb2a46b164424e1992e2fb232b79d7ee2
ducnt114 <trungduc110492@gmail.com>
2023-05-10 16:39:59 +0700

    update

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 ![Github Actions](https://github.com/ducnt114/g0database/actions/workflows/go.yml/badge.svg)
 
 A SQL database is written from scratch in Go
+
+## Lexer
+
+- Split raw sql command to token
+
+raw sql: `select id, name, age from user where status = 'ACTIVE' and id > 10`
+
+=> tokens: `["select", "id", ",", "name", ",", "age", "from", "user", "where", "status", "=", "'", "ACTIVE", "'", "and", "id", ">", "10"]`
+
+## Parser
+
+- Make Abstract syntax tree (AST) from list tokens
+
+```text
+root
+ |- select
+ |---- fields (id, name, age)
+ |- from
+ |---- tables (user)
+ |- where
+ |---- conditions
+           |---- status
+           |---- =
+           |---- ACTIVE
+       |- AND
+           |---- id
+           |---- >
+           |---- 10
+```
--- pkg/sql/lexer/naive_impl_test.go
+++ pkg/sql/lexer/naive_impl_test.go
@@ -22,15 +22,15 @@ func TestNaiveLexer_Analyze(t *testing.T) {
 	l := NewNaiveLexer()
 
 	t.Run("test case 1", func(t1 *testing.T) {
-		tokens, err := l.Analyze("select id, name, age from user where id > 10")
+		tokens, err := l.Analyze("select id, name, age from user where status = 'ACTIVE' and id > 10")
 		if err != nil {
 			t1.Fail()
 		}
-		for i, token := range tokens {
-			t1.Logf("tokens[%v]: %v", i, token)
-		}
-		if len(tokens) != 12 {
-			t1.Logf("expected len(tokens) = %v, actual: %v", 12, len(tokens))
+		//for i, token := range tokens {
+		//	t1.Logf("tokens[%v]: %v", i, token)
+		//}
+		if len(tokens) != 18 {
+			t1.Logf("expected len(tokens) = %v, actual: %v", 18, len(tokens))
 			t1.Fail()
 		}
 	})
@@ -43,9 +43,9 @@ func TestNaiveLexer_Analyze(t *testing.T) {
 		if err != nil {
 			t2.Fail()
 		}
-		for i, token := range tokens {
-			t2.Logf("tokens[%v]: %v", i, token)
-		}
+		//for i, token := range tokens {
+		//	t2.Logf("tokens[%v]: %v", i, token)
+		//}
 		if len(tokens) != 20 {
 			t2.Logf("expected len(tokens) = %v, actual: %v", 20, len(tokens))
 			t2.Fail()
@@ -60,20 +60,37 @@ func TestNaiveLexer_Analyze(t *testing.T) {
 		if err != nil {
 			t3.Fail()
 		}
-		for i, token := range tokens {
-			t3.Logf("tokens[%v]: %v", i, token)
-		}
+		//for i, token := range tokens {
+		//	t3.Logf("tokens[%v]: %v", i, token)
+		//}
 		if len(tokens) != 15 {
 			t3.Logf("expected len(tokens) = %v, actual: %v", 15, len(tokens))
 			t3.Fail()
 		}
 	})
 
+	t.Run("test case 4", func(t4 *testing.T) {
+		tokens, err := l.Analyze(`
+CREATE TABLE Employee_Info
+(
+EmployeeID int,
+EmployeeName varchar(255),
+Emergency ContactName varchar(255),
+PhoneNumber int,
+Address varchar(255),
+City varchar(255),
+Country varchar(255)
+);
+`)
+		if err != nil {
+			t4.Fail()
+		}
+		//for i, token := range tokens {
+		//	t4.Logf("tokens[%v]: %v", i, token)
+		//}
+		if len(tokens) != 27 {
+			t4.Logf("expected len(tokens) = %v, actual: %v", 27, len(tokens))
+			t4.Fail()
+		}
+	})
 }
-
-//func TestSlice(t *testing.T) {
-//	fmt.Println("id,"[0:2])
-//	fmt.Println("id,"[2:2])
-//	fmt.Println("id,"[1:2])
-//	fmt.Println("id,"[2:3])
-//}
