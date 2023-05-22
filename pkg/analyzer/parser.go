1c9d07fa4ff0531eca36116c1739ea63b4732858
ducnt114 <trungduc110492@gmail.com>
2023-05-22 12:08:51 +0700

    support parse command create

--- pkg/analyzer/parser.go
+++ pkg/analyzer/parser.go
@@ -2,6 +2,7 @@ package analyzer
 
 import (
 	"fmt"
+	"github.com/ducnt114/g0database/pkg/models"
 )
 
 type Parser interface {
@@ -82,8 +83,62 @@ func (p *parser) parseCommandUpdate(tokens []Token) (Command, error) {
 	return nil, fmt.Errorf("not implemented yet")
 }
 
+const (
+	createCommandStateCreate stateMachineType = iota
+	createCommandStateTable
+	createCommandStateBeginTable
+	createCommandStateEndTable
+	createCommandStateBeginColumnDefine
+	createCommandStateEndColumnDefine
+	createCommandStateColumnName
+	createCommandStateColumnType
+)
+
 func (p *parser) parseCommandCreate(tokens []Token) (Command, error) {
-	return nil, fmt.Errorf("not implemented yet")
+	res := &CommandCreate{
+		tableName: "",
+		columns:   make([]*models.Column, 0),
+	}
+	var state stateMachineType
+	var currentColumn *models.Column
+	for _, token := range tokens {
+		switch token {
+		case "create":
+			state = createCommandStateCreate
+			continue
+		case "table":
+			state = createCommandStateTable
+			continue
+		case "(":
+			if state == createCommandStateTable {
+				state = createCommandStateBeginTable
+			}
+			continue
+		case ")":
+			if state == createCommandStateBeginTable || state == createCommandStateColumnName {
+				state = createCommandStateEndTable
+			}
+		}
+		// got , when end of column define
+		if token == "," {
+			res.columns = append(res.columns, currentColumn)
+			state = createCommandStateBeginTable
+			continue
+		}
+		switch state {
+		case createCommandStateTable:
+			res.tableName = string(token)
+		case createCommandStateBeginTable:
+			currentColumn = &models.Column{}
+			currentColumn.Name = string(token)
+			state = createCommandStateColumnName
+		case createCommandStateColumnName:
+			currentColumn.Datatype = models.DataType(token)
+		case createCommandStateEndTable:
+			res.columns = append(res.columns, currentColumn)
+		}
+	}
+	return res, nil
 }
 func (p *parser) parseCommandInsert(tokens []Token) (Command, error) {
 	return nil, fmt.Errorf("not implemented yet")
