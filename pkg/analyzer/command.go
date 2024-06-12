fe30f75958061ec7593f00c561adf2a4b006eac0
ducnt114 <trungduc110492@gmail.com>
2024-06-12 18:16:22 +0700

    fix: pipeline

--- pkg/analyzer/command.go
+++ pkg/analyzer/command.go
@@ -1,36 +1,36 @@
 package analyzer
 
-import "github.com/ducnt114/g0database/pkg/models"
-
-type CommandType string
-
-const (
-	CommandTypeSelect CommandType = "select"
-	CommandTypeUpdate CommandType = "update"
-	CommandTypeDelete CommandType = "delete"
-	CommandTypeInsert CommandType = "insert"
-	CommandTypeCreate CommandType = "create"
-)
-
-type Command interface {
-	GetType() CommandType
-}
-
-type CommandSelect struct {
-	selectFields []string
-	fromTables   []string
-	conditions   []Condition
-}
-
-func (c *CommandSelect) GetType() CommandType {
-	return CommandTypeSelect
-}
-
-type CommandCreate struct {
-	tableName string
-	columns   []*models.Column
-}
-
-func (c *CommandCreate) GetType() CommandType {
-	return CommandTypeCreate
-}
+//import "github.com/ducnt114/g0database/pkg/models"
+//
+//type CommandType string
+//
+//const (
+//	CommandTypeSelect CommandType = "select"
+//	CommandTypeUpdate CommandType = "update"
+//	CommandTypeDelete CommandType = "delete"
+//	CommandTypeInsert CommandType = "insert"
+//	CommandTypeCreate CommandType = "create"
+//)
+//
+//type Command interface {
+//	GetType() CommandType
+//}
+//
+//type CommandSelect struct {
+//	selectFields []string
+//	fromTables   []string
+//	conditions   []Condition
+//}
+//
+//func (c *CommandSelect) GetType() CommandType {
+//	return CommandTypeSelect
+//}
+//
+//type CommandCreate struct {
+//	tableName string
+//	columns   []*models.Column
+//}
+//
+//func (c *CommandCreate) GetType() CommandType {
+//	return CommandTypeCreate
+//}
